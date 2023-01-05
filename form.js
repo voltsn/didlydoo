4776b38468bea3026d52680c3a74f4b4b894d577
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-01-05 15:52:40 +0100

    header.scss + form.js

---
+++ form.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector('.new-event-btn');
+console.log(btn);
+
+btn.addEventListener("click", showForm);
+
+function showForm() {
+    let form = document.querySelector('#event-form');
+    form.style.display = 'flex';
+}
