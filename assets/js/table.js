90d6265650d791ed8a41556e4d4c0b2fa2a39368
Milo Bonnet <milovtt@gmail.com>
2023-01-09 10:56:59 +0100

    Affichage de Name, desc, autheur du json sur html

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,10 +1,32 @@
-let submit = document.querySelector(".form-submit-btn")
+let submit = document.getElementById("form-submit-btn")
 
 submit.addEventListener('click', showPart);
 function showPart() {
-    fetch("../server/db/db.json")
+    fetch("http://localhost:3000/api/events")
     .then((response) => response.json())
     .then((text) => {
+        for (let i=0; i<=text.length; i++) {
+            const div = document.createElement('div');
 
+            const name = document.createElement('p')
+            const author = document.createElement('p');
+            const description = document.createElement('p');
+            const dates = document.createElement('p')
+
+            name.textContent = text[i].name;
+            author.textContent = text[i].author;
+            description.textContent = text[i].description;
+            dates.textContent;
+            for (let date of text[i].dates){
+                textContent = date.date;
+            }
+
+            div.appendChild(name)
+            div.appendChild(author); 
+            div.appendChild(description);
+            div.appendChild(document.createTextNode(textContent));
+            
+            document.body.appendChild(div);
+        }
     });
 };
