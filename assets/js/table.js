b48389167c53993239cf202c9e879750bf6880af
Milo Bonnet <milovtt@gmail.com>
2023-01-09 12:29:01 +0100

    Affichage dates

--- assets/js/table.js
+++ assets/js/table.js
@@ -6,26 +6,30 @@ function showPart() {
     .then((response) => response.json())
     .then((text) => {
         for (let i=0; i<=text.length; i++) {
+
+            // Création de la div
             const div = document.createElement('div');
 
+            // Création de nos paragraphes avec nos éléments du json
             const name = document.createElement('p')
-            const author = document.createElement('p');
             const description = document.createElement('p');
-            const dates = document.createElement('p')
+            const author = document.createElement('p');
+            const dates = document.createElement('p');
 
             name.textContent = text[i].name;
-            author.textContent = text[i].author;
             description.textContent = text[i].description;
+            author.textContent = text[i].author;
             dates.textContent;
+
+            div.appendChild(name);
+            div.appendChild(description);
+            div.appendChild(author); 
+            
             for (let date of text[i].dates){
                 textContent = date.date;
+                div.appendChild(document.createTextNode(textContent));
             }
 
-            div.appendChild(name)
-            div.appendChild(author); 
-            div.appendChild(description);
-            div.appendChild(document.createTextNode(textContent));
-            
             document.body.appendChild(div);
         }
     });
